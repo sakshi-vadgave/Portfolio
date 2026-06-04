5cc7865cfe6937c54615d17108a17ab28a60a659
Sakshi Vadgave <vadgavesakshi8@gmail.com>
2026-06-04 17:25:26 +0530

    fixed backend

--- api/chat.js
+++ api/chat.js
@@ -1,16 +1,29 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    }
-  );
+  try {
+    const userMessage = req.body;
 
-  const data = await response.json();
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userMessage),
+      }
+    );
 
-  res.status(200).json(data);
+    const data = await response.json();
+
+    console.log(data);
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 }
